a73726f1ed9a616fea2e0d0ee5275c7fd461aa18
nyeinchan <aungNyeinChan.dev@gmail.com>
2024-11-22 18:47:21 +0630

    tailwind css tuto

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        extra: "5rem",
+        header: "2rem",
+      },
+      colors: {
+        primary: "red",
+        secondary: {
+          100: "green",
+          200: "blue",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
